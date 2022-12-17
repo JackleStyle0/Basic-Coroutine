f5a9509050d3fe7e7bdeca9fb933bc1ba524d6e8
Jckl3 <wacharat@odds.team>
2022-12-17 16:50:44 +0700

    Example dependent job

--- src/main/kotlin/ch3_getting_started_with_coroutine/Ch3Main.kt
+++ src/main/kotlin/ch3_getting_started_with_coroutine/Ch3Main.kt
@@ -1,13 +1,33 @@
 package ch3_getting_started_with_coroutine
 
 import kotlinx.coroutines.*
-import kotlin.concurrent.thread
-import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
 fun main() {
 //    startFirstCoroutine()
-    explainingJobs()
+//    explainingJobs()
+    dependentJobs()
+}
+
+/*
+    This is launching my first coroutine with loop from 1 to 10000 which
+    most program would get an OutOfMemoryException
+    but for the coroutine able to launch a large number of 10000 thread without any performance impact
+    it have 2 important in this function about launching coroutine
+        1.block of code passed as the parameter to launch(), which is called s "coroutine builder"
+        2.GlobalScope provide to coroutine because they are background mechanisms which don't care about lifecycle
+          sometime program finish before coroutine completed. It this function use GlobalScope the coroutine lifecycle
+          is bound to the lifecycle of the application
+ */
+fun startFirstCoroutine() {
+    (1..10000).forEach {
+        val scope = CoroutineScope(EmptyCoroutineContext)
+        GlobalScope.launch {
+            val threadName = Thread.currentThread().name
+            println("$it printed on Thread ${threadName}")
+        }
+        Thread.sleep(1000)
+    }
 }
 
 /* Job
@@ -38,23 +58,31 @@ fun explainingJobs() {
     }
 }
 
-/*
-    This is launching my first coroutine with loop from 1 to 10000 which
-    most program would get an OutOfMemoryException
-    but for the coroutine able to launch a large number of 10000 thread without any performance impact
-    it have 2 important in this function about launching coroutine
-        1.block of code passed as the parameter to launch(), which is called s "coroutine builder"
-        2.GlobalScope provide to coroutine because they are background mechanisms which don't care about lifecycle
-          sometime program finish before coroutine completed. It this function use GlobalScope the coroutine lifecycle
-          is bound to the lifecycle of the application
+/* Dependent Jobs
+    Define jobPong start coroutine Lazy mode
+    jobPong will execute when invoke join or start
+    so when jobPong.join() next line will be execute until execute of jobPong completed
+    Expect output
+    PING
+    PONG
+    PING
  */
-fun startFirstCoroutine() {
-    (1..10000).forEach {
-        val scope = CoroutineScope(EmptyCoroutineContext)
-        GlobalScope.launch {
-            val threadName = Thread.currentThread().name
-            println("$it printed on Thread ${threadName}")
-        }
-        Thread.sleep(1000)
+fun dependentJobs() {
+    val jobPong = GlobalScope.launch(start = CoroutineStart.LAZY) {
+        delay(200)
+        println("PONG")
+        delay(200)
     }
+
+    GlobalScope.launch {
+        delay(200)
+        println("PING")
+        println("Job pong status ${jobPong.isCompleted}")
+        jobPong.join()
+        println("Job pong status ${jobPong.isCompleted}")
+        println("PING")
+        delay(200)
+    }
+
+    Thread.sleep(1000)
 }
